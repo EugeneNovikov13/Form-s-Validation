e2e6d40b1bbf820fd7277b7d0ef2fa773c734c52
Novikov Eugene <evgeniy.novikov@neuro-city.ru>
2023-06-11 16:24:13 +0300

    добавлена валидация и переход фокуса на submit

--- form/src/app.js
+++ form/src/app.js
@@ -1,3 +1,4 @@
+import { useRef, useEffect } from 'react';
 import { useStore } from './hooks/useStore';
 import styles from './app.module.css';
 
@@ -9,15 +10,32 @@ export const App = () => {
 	const { getState, getErrors, updateState, updateErrorsState, resetState } =
 		useStore();
 
+	const { email, password, passwordRepeat } = getState();
+
+	const { emailError, passwordError, passwordRepeatError } = getErrors();
+
+	const formError = Object.values(getErrors()).some(el => el);
+
+	const emailRef = useRef(null);
+
+	const submitButtonRef = useRef(null);
+
+	useEffect(() => {
+		if (
+			/^[\w-.]+@([\w-]+\.)+[\w-]{2,}$/.test(email) &&
+			/(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}/g.test(password) &&
+			password === passwordRepeat &&
+			!formError
+		)
+			submitButtonRef.current.focus();
+	}, [email, password, passwordRepeat, formError]);
+
 	const onSubmit = event => {
 		event.preventDefault();
 		sendData(getState());
 		resetState();
+		emailRef.current.focus();
 	};
-
-	const { email, password, passwordRepeat } = getState();
-
-	const { emailError, passwordError, passwordRepeatError } = getErrors();
 
 	const onChange = ({ target }) => {
 		updateState(target.name, target.value);
@@ -32,9 +50,27 @@ export const App = () => {
 					: updateErrorsState('emailError', '');
 				break;
 			}
+			case 'password': {
+				if (!target.value.length) break;
+				!/^[A-Za-z0-9]+$/.test(target.value)
+					? updateErrorsState(
+							'passwordError',
+							'Только латинские буквы или цифры',
+					  )
+					: updateErrorsState('passwordError', '');
+				break;
+			}
+			case 'passwordRepeat': {
+				updateErrorsState('passwordRepeatError', '');
+				break;
+			}
 			default: {
 			}
 		}
+	};
+
+	const onFocusPassword = () => {
+		updateState('passwordRepeat', '');
 	};
 
 	const onBlur = ({ target }) => {
@@ -48,12 +84,40 @@ export const App = () => {
 					: updateErrorsState('emailError', '');
 				break;
 			}
+			case 'password': {
+				if (!/[a-z]+/.test(target.value)) {
+					updateErrorsState(
+						'passwordError',
+						'Должна быть хотя бы одна латинская буква',
+					);
+				} else if (!/[A-Z]+/.test(target.value)) {
+					updateErrorsState(
+						'passwordError',
+						'Должна быть хотя бы одна большая буква',
+					);
+				} else if (!/\d+/.test(target.value)) {
+					updateErrorsState(
+						'passwordError',
+						'Пароль должен содержать хотя бы одну цифру',
+					);
+				} else if (target.value.length < 8) {
+					updateErrorsState(
+						'passwordError',
+						'Пароль должен быть не менее 8 символов',
+					);
+				} else updateErrorsState('passwordError', '');
+				break;
+			}
+			case 'passwordRepeat': {
+				target.value !== password
+					? updateErrorsState('passwordRepeatError', 'Пароли должны совпадать')
+					: updateErrorsState('passwordRepeatError', '');
+				break;
+			}
 			default: {
 			}
 		}
 	};
-
-	let formError = Object.values(getErrors()).some(el => el);
 
 	return (
 		<div className={styles.app}>
@@ -61,6 +125,7 @@ export const App = () => {
 				<label htmlFor="email" className={styles.label}>
 					{emailError}
 					<input
+						ref={emailRef}
 						id="email"
 						type="email"
 						name="email"
@@ -69,10 +134,12 @@ export const App = () => {
 						value={email}
 						onChange={onChange}
 						onBlur={onBlur}
+						autoFocus
+						required
 					/>
 				</label>
 				<label htmlFor="password" className={styles.label}>
-					Введите пароль
+					{passwordError}
 					<input
 						id="password"
 						type="password"
@@ -81,10 +148,13 @@ export const App = () => {
 						placeholder="Пароль"
 						value={password}
 						onChange={onChange}
+						onFocus={onFocusPassword}
+						onBlur={onBlur}
+						required
 					/>
 				</label>
 				<label htmlFor="passwordRepeat" className={styles.label}>
-					Повторите пароль
+					{passwordRepeatError}
 					<input
 						id="passwordRepeat"
 						type="password"
@@ -93,12 +163,19 @@ export const App = () => {
 						placeholder="Повтор пароля"
 						value={passwordRepeat}
 						onChange={onChange}
+						onBlur={onBlur}
+						required
 					/>
 				</label>
 				<button type="button" className={styles.button} onClick={resetState}>
 					Сбросить данные
 				</button>
-				<button type="submit" className={styles.button} disabled={!!formError}>
+				<button
+					ref={submitButtonRef}
+					type="submit"
+					className={styles.button}
+					disabled={!!formError}
+				>
 					Зарегистрироваться
 				</button>
 			</form>
